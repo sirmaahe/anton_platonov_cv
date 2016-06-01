d04e453678f357ebecf6164d03c5f37988695158
turbo_jesus <sirmaahe@gmail.com>
2016-06-01 22:27:52 +0300

    {для лаб}

--- app/views.py
+++ app/views.py
@@ -7,5 +7,9 @@ from app import app
 def index():
     return render_template('index.html')
 
+@app.route('/cookies', methods=['GET'])
+def cookies():
+    return render_template('cookies.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
