f88fe8d9f9e093d21d33a688141ee7ebe704c442
turbo_jesus <sirmaahe@gmail.com>
2016-06-06 21:37:40 +0300

    {для лаб}

--- app/views.py
+++ app/views.py
@@ -7,13 +7,20 @@ from app import app
 def index():
     return render_template('index.html')
 
+
 @app.route('/cookies', methods=['GET'])
 def cookies():
     return render_template('cookies.html')
 
+
 @app.route('/zeynalov/cookies', methods=['GET'])
 def zeynalov_cookies():
     return render_template('zeynalov/cookies.html')
+
+
+@app.route('/zeynalov/resume', methods=['GET'])
+def zeynalov_resume():
+    return render_template('zeynalov/resume.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
